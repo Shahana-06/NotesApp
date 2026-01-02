c831b6b1eb2fbbc4798d2aebd1bdf715d9448c37
Shahana-06 <Shahanasuresh2006@gmail.com>
2026-01-02 13:34:08 +0530

    updated db

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 from fastapi import FastAPI
 from config import session, engine 
+from model import Notes
 import db_model
-
-db_model.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
-# in-memory storage of data
-notes = []
+#To create table
+db_model.Base.metadata.create_all(bind = engine)
+
+notes = [
+    Notes (note_id = 1, note_name="To do list", note_content="1. Update Github\n2. Update Resume\n3. Brush up concepts\n4. Call Parents"), 
+    Notes (note_id = 2, note_name= "Things to pack", note_content="1. Laptop, charger\n2. Visa, Passport\n3. Offer Letter"),
+]
+
+def init_db ():
+    db = session()
+
+    count = db.query (db_model.Notes).count
+
+    if count==0:
+        for note in notes:
+            db.add(db_model.Notes(**note.model_dump()))
+
+    db.commit()
+
+init_db()
 
 @app.get("/")
 def home():
     return {"message": "Hello World"}
 
 @app.get("/notes")
-def get_notes(notes: str):
-    db = session ()
-    return {"notes": notes}
+def get_notes():
+    # db = session ()
+    return notes
 
 @app.post("/notes")
 def create_note(title: str, content: str):
-    note = {"id": len(notes) + 1, "title": title, "content": content}
-    notes.append(note)
+    note = Notes(note_id= len(notes) + 1, note_name= title, note_content= content)
+    # notes.append(note)
+    db.add(db_model.Notes(**note.model_dump()))
     return note
 
 @app.delete("/notes/{note_id}")
