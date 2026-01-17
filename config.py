9d1db249d257aba1f45d207feb9d25b9bc65449b
Shahana-06 <shahanasuresh2006@gmail.com>
2026-01-17 09:18:59 +0530

    Update DB_PASSWORD to use a placeholder value

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Database configuration
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Shan1234$$")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "...")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "shahanas")
